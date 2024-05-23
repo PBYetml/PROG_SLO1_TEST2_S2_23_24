cac525208cbf1b0f818b6b012ec45faee9017ef6
ETMLNET\etideoliveira <etienne.deoliveira@etml-es.ch>
2024-05-23 10:25:40 +0200

    Correction question 3 faite

--- EDO/Test 3 Prog_EDO/Test3/Test3/Q3.c
+++ EDO/Test 3 Prog_EDO/Test3/Test3/Q3.c
@@ -19,17 +19,30 @@
 void main()
 {
 	const char* pt_message = TITRE;
-	const char tbMSG[] = { 0x53, 0x4C, 0x4F};
+	const char tbMSG[] = { 0x53, 0x4C, 0x4F };
 	char tbMSGComplet[15];
-	int annee = 2022;
-	int8_t version = 0x33;
-	char Chaine1[10];
+	int annee = 2024;
+	char version = 0x33;
+	char Chaine1[5];
 
-	
-	//printf("avant copie =%s \n", pt_message);
-	strcpy(Chaine1, &tbMSG[0]);
-	printf("apres copie = %s \n", Chaine1);
+	//Mes instructions
 
-	strcat(Chaine1, pt_message);
-	printf("strcat = % s \n", Chaine1);
+	tbMSGComplet[0] = '\0';
+
+	strcpy(tbMSGComplet, TITRE);
+
+	int lenght = strlen(tbMSGComplet);
+	tbMSGComplet[lenght] = version;
+	tbMSGComplet[lenght + 1] = '\0';
+
+	strcat(tbMSGComplet, " ");
+
+	strncat(tbMSGComplet, tbMSG, 3);
+	itoa(annee, Chaine1, 10);
+
+	strcat(tbMSGComplet, Chaine1);
+
+	printf("%s \n", tbMSGComplet);
+
+	return 0;
 }
